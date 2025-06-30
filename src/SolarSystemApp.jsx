aeab4e23d18148b80cd80a059ecd7363a15fc5f0
Shad-Hassan <shadhassan8991@gmail.com>
2025-06-30 11:08:52 +0600

    behold the cosmos

---
+++ src/SolarSystemApp.jsx
@@ -0,0 +1,13 @@
+
+
+const SolarSystemApp = () => {
+  return(
+    <>
+     <div>
+          <h1 className="text-red-500 text-5xl">Behold The Cosmos</h1>
+      </div>
+    </>
+  )
+}
+
+export default SolarSystemApp
